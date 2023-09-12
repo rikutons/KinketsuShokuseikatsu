16656043784a7dda465ed9666778db7fd47ebe55
rikutons <rikutonsicloud@gmail.com>
2023-09-12 16:20:14 +0900

    aaa

--- api/index.js
+++ api/index.js
@@ -5,25 +5,25 @@ const config = {
   channelSecret:  process.env.SECRET,
   recruitKey:  process.env.RECRUIT_APIKEY
 };
+const { response } = require("express")
 
 const axios = require("axios").default;
 const PORT = process.env.PORT || 3000;
 
-// (process.env.NOW_REGION) ? module.exports = app : app.listen(PORT);
-// console.log(`Server running at ${PORT}`);
+const client = new line.Client(config);
 const app = express()
+
 app.get('/', (req, res) => res.send('Hello LINE BOT!(GET)')); //ブラウザ確認用(無くても問題ない)
 app.post('/webhook', line.middleware(config), (req, res) => {
     Promise
       .all(req.body.events.map(handleEvent))
       .then((result) => res.json(result))
       .catch((err) => {
- 	    console.error(err);
+        console.error(err);
         res.status(500).end();
       });
 });
 
-const client = new line.Client(config);
 
 async function handleEvent(event) {
   if (event.message.type !== 'location') {
@@ -45,25 +45,21 @@ async function handleEvent(event) {
       "format" : "json"
     }
   })
-  return client.replyMessage(event.replyToken, {
-    type: 'text',
-    text: '位置情報ありがとう！'
-  })
   await instance.get().then((response) => {
-    // const results = response.data.results;
-    // console.log(JSON.stringify(response.data));
-    // console.log(JSON.stringify(results));
-    // var storeList = []
-    // // jsonデータをリストに格納
-    // results.shop.forEach((storeData) => {
-    //   var store = {
-    //     name: storeData.name,
-    //     hotpepper: storeData.urls.pc,
-    //     image: storeData.photo.pc.l
-    //   }
-    //   storeList.push(store);
-    // })
-    // const theStore = storeList[Math.floor(Math.random() * storeList.length)];
+    const results = response.data.results;
+    console.log(JSON.stringify(response.data));
+    console.log(JSON.stringify(results));
+    var storeList = []
+    // jsonデータをリストに格納
+    results.shop.forEach((storeData) => {
+      var store = {
+        name: storeData.name,
+        hotpepper: storeData.urls.pc,
+        image: storeData.photo.pc.l
+      }
+      storeList.push(store);
+    })
+    const theStore = storeList[Math.floor(Math.random() * storeList.length)];
     const message = {
       type: "text",
       text: "scoop test"
